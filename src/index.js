0f5b9f790d1bded11995eaa4c75c5cf9b9e17153
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-16 21:59:56 +0530

    Added controller

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDB from './config/dbconfig.js';
+import { createPost } from './controllers/postController.js';
 
 const PORT = 3000; // PORT number
 
@@ -8,6 +9,8 @@ const app = express(); // create express app server instance
 app.get('/ping', (req, res) => {
     return res.json({message : 'Pong'});
 });
+
+app.post('/posts', createPost);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);

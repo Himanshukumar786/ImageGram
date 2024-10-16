ddb1385f8dc7a199b4d1303847ad2e421e1a2bf5
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-17 00:10:24 +0530

    Added middleware demo

--- src/index.js
+++ src/index.js
@@ -6,26 +6,16 @@ const PORT = 3000; // PORT number
 
 const app = express(); // create express app server instance
 
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+
 app.get('/ping', (req, res) => {
-    return res.json({message : 'Pong'});
+    console.log(req.query);
+    console.log(req.body);
 });
 
-function m1(req, res, next){
-    console.log('m1');
-    next();
-}
-
-function m2(req, res, next){
-    console.log('m2');
-    next();
-}
-
-function m3(req, res, next){
-    console.log('m3');
-    next();
-}
-
-app.post('/posts', m1, m2, m3, createPost);
+app.post('/posts', createPost);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
